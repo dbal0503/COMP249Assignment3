30353b4cc693c36aa5ac913bc03633d7b13b3e16
MatthewTP0 <113402358+MatthewTP0@users.noreply.github.com>
2023-08-04 22:59:54 -0400

    committing toaster

--- Book.java
+++ Book.java
@@ -1,2 +1,88 @@
 public class Book {
+    private String title;
+    private String author;
+    private double price;
+    private long ISBN;
+    private String genre;
+    private int year;
+
+    public Book(){
+
+    }
+    public Book(String title, String author, double price, long ISBN, String genre, int year){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.ISBN = ISBN;
+        this.genre = genre;
+        this.year = year;
+
+    }
+    public Book(Book book){
+        this.title = book.title;
+        this.author = book.author;
+        this.price = book.price;
+        this.ISBN = book.ISBN;
+        this.year = book.year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", ISBN=" + ISBN +
+                ", genre='" + genre + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,83 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
 public class Main {
-    public static void main(String[] args){
+
+
+
+    public static Book createBookfromString(String[] temp){
+        for (int i =0; i<temp.length; i++){
+            temp[i] = temp[i].trim();
+        }
+        return new Book(temp[0],temp[1],Double.parseDouble(temp[2]),Long.parseLong(temp[3]),temp[4],Integer.parseInt(temp[5]));
+    }
+
+
+    public static Object[] collect(BookList bkLst,ArrayList<Book> arrLst ) {
+        String[] temp;
+        Book aBook;
+        Object[] objects = new Object[2];
+        try {
+            Scanner read = new Scanner(new FileInputStream("Books.txt"));
+
+
+            while (read.hasNextLine()) {
+                temp = read.nextLine().split(",");
+                for (int i =0; i< temp.length; i++){
+                    System.out.println(temp[i]);
+                }
+
+                if ((Integer.parseInt(temp[5].trim()) > 1900) && (Integer.parseInt(temp[5].trim()) < 2023)) {
+
+                    bkLst.addToStart(createBookfromString(temp));
+
+                } else {
+
+
+                    arrLst.add(createBookfromString(temp));
+                }
+
+            }
+
+            read.close();
+        }
+        catch (IOException ioException){
+            System.out.println(ioException.getMessage());
+        }
+        objects[0] = bkLst;
+        objects[1] = arrLst;
+        return objects;
+    }
+
+
+
+    public static void main(String[] args) {
+        File file = new File("2008.txt");
+        file.delete();
+
+        BookList bkLst = new BookList();
+        ArrayList<Book> arrLst = new ArrayList<Book>();
+        Object[] objects = collect(bkLst, arrLst);
+        bkLst = (BookList) objects[0];
+        arrLst = (ArrayList<Book>) objects[1];
+
+        for (int i = 0; i < arrLst.size(); i++) {
+            System.out.println(arrLst.get(i).toString());
+        }
+        bkLst.show();
+        bkLst.storeRecordsByYear(2008);
+        Book booktoinsert = new Book("The Dark Road", "Jimin S.", 25.92, 1239009879, "FCN", 2019);
+        long longISBN = 97807864121l;
+        long longISBN2 = 97807862907l;
+        System.out.println(bkLst.insertBetween(longISBN, longISBN2, booktoinsert));
+        //System.out.println(bkLst.insertBefore(807863614, booktoinsert));
+        bkLst.show();
+
+        float fol = 10f;
+        long lol = 10l;
 
     }
-}
+    }
